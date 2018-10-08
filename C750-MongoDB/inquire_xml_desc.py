4b9f471198004a850a5c762f225e65bef6d1062b
Michael Jenkins <mdjenkin1@gmail.com>
2018-10-08 06:38:58 -0600

    troubleshooting why it breaks on large xml.

---
+++ C750-MongoDB/inquire_xml_desc.py
@@ -0,0 +1,13 @@
+"""
+Find answers to generic questions about xml metadata.
+xml metadata is assumed to be loaded to mongodb
+"""
+
+## Which elements have attributes with more than one data type.
+## Which elements are nested in other elements
+
+def test():
+    pass
+
+if __name__ == "__main__":
+    test()
--- C750-MongoDB/xml_desc_to_mongo.py
+++ C750-MongoDB/xml_desc_to_mongo.py
@@ -1,46 +1,20 @@
 """
-Generic script to insert xml described  MongoDB listening on the default port
-
-script from lesson
-#!/usr/bin/env python
-
-Add a single line of code to the insert_autos function that will insert the
-automobile data into the 'autos' collection. The data variable that is
-returned from the process_file function is a list of dictionaries, as in the
-example in the previous video.
-
-
-from autos import process_file
-
-
-def insert_autos(infile, db):
-	data = process_file(infile)
-	# Add your code here. Insert the data in one command.
-	for item in data: db.autos.insert(item)
-  
-if __name__ == "__main__":
-	# Code here is for local use on your own computer.
-	from pymongo import MongoClient
-	client = MongoClient("mongodb://localhost:27017")
-	db = client.examples
-
-	insert_autos('autos-small.csv', db)
-	print(db.autos.find_one())
-
+Generic script to insert xml description to local MongoDB listening on the default port
 """
+
 import describe_xml as dxml
 import pprint
 from pymongo import MongoClient
 
 def insert_to_db(data, db, doc):
     for item in data: 
-        db[doc].insert(item)
+        db[doc].insert_one(item)
 
 def connect_db(host="localhost", port=27017):
     client = MongoClient(host, port)
     return client
 
-def test(infile = 'map', doc = 'map'):
+def test(infile = 'map.small.snip', doc = 'map'):
 	client = connect_db()
 	db = client['xml_descriptions']
 	xml_desc = dxml.get_xml_description(infile)
