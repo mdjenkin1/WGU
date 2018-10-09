2de0b8ad861d9fd199e22eb2356654224d748aa2
Michael Jenkins <mdjenkin1@gmail.com>
2018-10-09 05:57:41 -0600

    removed unnecessary function

--- C750-MongoDB/xml_desc_to_mongo.py
+++ C750-MongoDB/xml_desc_to_mongo.py
@@ -6,19 +6,15 @@ import describe_xml as dxml
 import pprint
 from pymongo import MongoClient
 
-def insert_to_db(data, db, doc):
-    for item in data: 
-        db[doc].insert_one(item)
-
 def connect_db(host="localhost", port=27017):
     client = MongoClient(host, port)
     return client
 
-def test(infile = 'map.small.snip', doc = 'map'):
+def test(infile = 'map', db_name = 'xml_descriptions', coll = 'map'):
 	client = connect_db()
-	db = client['xml_descriptions']
+	db = client[db_name]
 	xml_desc = dxml.get_xml_description(infile)
-	insert_to_db(xml_desc['elements'], db, doc)
+	db[coll].insert_many(xml_desc['elements'])
 
 if __name__ == "__main__":
 	test()
