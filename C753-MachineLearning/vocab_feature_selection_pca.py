686a4c4dc79cdc4fb772a98594b8b1e1844f981a
Michael Jenkins <mdjenkin1@gmail.com>
2019-08-04 20:21:17 -0600

    Added term matrix for each person.

--- C753-MachineLearning/vocab_feature_selection_pca.py
+++ C753-MachineLearning/vocab_feature_selection_pca.py
@@ -4,7 +4,14 @@
     https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
     
     Each word stem is a vocabulary feature. The TfIdf weight of that stem directly relates to its weight in the vocabulary.
-    Each vocabulary will be a 
+    
+    Construct a panda's dataframe with the following Structure:
+    Each column is a stem/feature
+    Each row is a person
+    Each 
+
+
+    Each person's vocabulary will be an observation in the dataframe
 
 
 """
@@ -13,12 +20,23 @@ import sys
 import os
 import pickle
 import pandas as pd
+import numpy as np
 
-
-person_vocabs = pickle.load(open('.\preprocessed_email_dump_funsize.pkl','rb'))
+persons_vocabs = pickle.load(open('.\preprocessed_email_dump_funsize.pkl','rb'))
 #person_vocabs = pickle.load(open('.\preprocessed_email_dump.pkl','rb'))
 
-# Load the data to a pandas dataframe
+### Structure the data to be loaded to a pandas dataframe
+
+# obtain a list of all stems as features. These are the column headers
+word_counts = collections.Counter()
+for person in persons_vocabs:
+    word_counts.update(persons_vocabs[person].get_feature_names())
+    stem_weights = {}
+
+
+cols = word_counts.keys()
+vocabs_df = pd.df()
+
 
 # Standardize the data
 
