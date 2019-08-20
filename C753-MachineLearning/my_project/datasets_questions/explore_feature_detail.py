e0a3d93466cc994763fdc2be35bee66c64ca46cc
Michael Jenkins <mdjenkin1@gmail.com>
2019-08-20 08:40:50 -0600

    dataframe descriptions with zeros.

--- C753-MachineLearning/my_project/datasets_questions/explore_feature_detail.py
+++ C753-MachineLearning/my_project/datasets_questions/explore_feature_detail.py
@@ -19,19 +19,32 @@ cleaned_data = pickle.load(open("../pickle_jar/final_project_dataset_cleaned.pkl
 
 #pprint.pprint(cleaned_data)
 
-
 fin_features = ['poi','salary', 'deferral_payments', 'total_payments', 'bonus', 
     'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 
     'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']
 
-email_features = ['poi', 'to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 
+email_features = ['poi', 'to_messages', 'from_poi_to_this_person', 'from_messages', 
     'from_this_person_to_poi', 'shared_receipt_with_poi'] 
 
 fin_data = featureFormat(cleaned_data, fin_features)
-_, finance_features = targetFeatureSplit( fin_data )
+_, fin_data_np_arrays = targetFeatureSplit( fin_data )
+fin_data_df = pd.DataFrame(fin_data_np_arrays, columns = fin_features[1:])
 
-
+email_data = featureFormat(cleaned_data, email_features)
+_, email_data_np_arrays = targetFeatureSplit( email_data )
+email_data_df = pd.DataFrame(email_data_np_arrays, columns = email_features[1:])
 
 #pprint.pprint(type(fin_data))
-pprint.pprint(type(finance_features))
-#pprint.pprint(finance_features)
+#pprint.pprint(type(fin_data_np_arrays))
+#pprint.pprint(fin_data_np_arrays)
+#pprint.pprint(fin_data_np_arrays)
+#pprint.pprint(fin_data_df)
+#pprint.pprint(email_data_df)
+
+#print(type(fin_data_df))
+#print(type(email_data_df))
+
+pd.set_option('precision',2)
+
+pprint.pprint(fin_data_df.describe())
+pprint.pprint(email_data_df.describe())
