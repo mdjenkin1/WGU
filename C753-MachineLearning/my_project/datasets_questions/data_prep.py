1e098fb50c1c89227a5662b3c3c9129ad80a0362
Michael Jenkins <mdjenkin1@gmail.com>
2019-08-21 06:39:14 -0600

    Corrected data_prep dynamic entry droping

--- C753-MachineLearning/my_project/datasets_questions/data_prep.py
+++ C753-MachineLearning/my_project/datasets_questions/data_prep.py
@@ -13,13 +13,28 @@ cleaned_ef_data = {}
 # Static entries for dropping
 drop_person = set(['THE TRAVEL AGENCY IN THE PARK', 'TOTAL'])
 
+# Dynamic entry dropping
+for person in original_ef_data:
+    # mise name normalization
+    norm_person = re.sub(r'\s\b\w\b| JR|\.','',person).upper()
+    original_ef_data[norm_person] = original_ef_data.pop(person)
+    # Drop those with no value
+    has_value = False
+    for feature in original_ef_data[norm_person]:
+        if feature != 'poi' and original_ef_data[norm_person][feature] != 'NaN':
+            has_value = True
+            break
+    if not has_value: 
+        drop_person.add(norm_person)
+
+#print(drop_person)
+
 # Copy and clean our data
 for person in original_ef_data:
-    norm_person = re.sub(r'\s\b\w\b| JR|\.','',person).upper()
-    if norm_person not in drop_person or len(original_ef_data[person].keys()) <= 1:
-        cleaned_ef_data[norm_person] = {feat:original_ef_data[person][feat] for feat in original_ef_data[person] if feat!='loan_advances'}
+    if person not in drop_person:
+        cleaned_ef_data[person] = {feat:original_ef_data[person][feat] for feat in original_ef_data[person] if feat!='loan_advances'}
         if original_ef_data[person]['loan_advances'] != 'NaN':
-            cleaned_ef_data[norm_person]['total_payments'] = cleaned_ef_data[norm_person]['total_payments'] - original_ef_data[person]['loan_advances']
+            cleaned_ef_data[person]['total_payments'] = cleaned_ef_data[person]['total_payments'] - original_ef_data[person]['loan_advances']
 
 out = open("../pickle_jar/final_project_dataset_cleaned.pkl","wb")
 pickle.dump(cleaned_ef_data, out)
